9d0446ce956637c81ec11afd0c4554e7977f76cd
saicharanpogul <pogul1804@gmail.com>
2022-04-26 02:56:34 +0530

    Discarded 'terser'

--- ts/rollup.config.ts
+++ ts/rollup.config.ts
@@ -2,7 +2,6 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import replace from "@rollup/plugin-replace";
 import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
 
 const env = process.env.NODE_ENV;
 
@@ -30,7 +29,6 @@ export default {
         "process.env.BROWSER": JSON.stringify(true),
       },
     }),
-    terser(),
   ],
   external: [
     "@project-serum/borsh",
